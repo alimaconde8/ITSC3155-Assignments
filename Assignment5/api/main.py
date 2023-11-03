55b97c594f0fd85dbed66f9dc3cc9b785abaef2a
alimaconde8 <aconde4@uncc.edu>
2023-11-02 21:50:46 -0400

    feat: add assignment files

--- Assignment5/api/main.py
+++ Assignment5/api/main.py
@@ -1,55 +1,52 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from fastapi.middleware.cors import CORSMiddleware
 
-from .models import models, schemas
-from .controllers import orders
-from .dependencies.database import engine, get_db
-
-models.Base.metadata.create_all(bind=engine)
+from . import schemas
+from .controllers import sandwiches as sandwiches_controller
+from .dependencies import database
 
 app = FastAPI()
 
-origins = ["*"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.post("/orders/", response_model=schemas.Order, tags=["Orders"])
-def create_order(order: schemas.OrderCreate, db: Session = Depends(get_db)):
-    return orders.create(db=db, order=order)
+# Dependency to get the database session
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.get("/orders/", response_model=list[schemas.Order], tags=["Orders"])
-def read_orders(db: Session = Depends(get_db)):
-    return orders.read_all(db)
+# Sandwiches Endpoints
+@app.post("/sandwiches/", response_model=schemas.Sandwich, tags=["sandwiches"])
+def create_sandwich(sandwich: schemas.SandwichCreate, db: Session = Depends(get_db)):
+    return sandwiches_controller.create_sandwich(db=db, sandwich=sandwich)
 
 
-@app.get("/orders/{order_id}", response_model=schemas.Order, tags=["Orders"])
-def read_one_order(order_id: int, db: Session = Depends(get_db)):
-    order = orders.read_one(db, order_id=order_id)
-    if order is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return order
+@app.get("/sandwiches/", response_model=list[schemas.Sandwich], tags=["sandwiches"])
+def read_all_sandwiches(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return sandwiches_controller.read_all_sandwiches(db=db).offset(skip).limit(limit).all()
 
 
-@app.put("/orders/{order_id}", response_model=schemas.Order, tags=["Orders"])
-def update_one_order(order_id: int, order: schemas.OrderUpdate, db: Session = Depends(get_db)):
-    order_db = orders.read_one(db, order_id=order_id)
-    if order_db is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return orders.update(db=db, order=order, order_id=order_id)
+@app.get("/sandwiches/{sandwich_id}", response_model=schemas.Sandwich, tags=["sandwiches"])
+def read_one_sandwich(sandwich_id: int, db: Session = Depends(get_db)):
+    db_sandwich = sandwiches_controller.read_one_sandwich(db=db, sandwich_id=sandwich_id)
+    if db_sandwich is None:
+        raise HTTPException(status_code=404, detail="Sandwich not found")
+    return db_sandwich
 
 
-@app.delete("/orders/{order_id}", tags=["Orders"])
-def delete_one_order(order_id: int, db: Session = Depends(get_db)):
-    order = orders.read_one(db, order_id=order_id)
-    if order is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return orders.delete(db=db, order_id=order_id)
+@app.put("/sandwiches/{sandwich_id}", response_model=schemas.Sandwich, tags=["sandwiches"])
+def update_sandwich(sandwich_id: int, sandwich: schemas.SandwichUpdate, db: Session = Depends(get_db)):
+    db_sandwich = sandwiches_controller.read_one_sandwich(db=db, sandwich_id=sandwich_id)
+    if db_sandwich is None:
+        raise HTTPException(status_code=404, detail="Sandwich not found")
+    return sandwiches_controller.update_sandwich(db=db, sandwich_id=sandwich_id, sandwich=sandwich)
+
+
+@app.delete("/sandwiches/{sandwich_id}", response_model=schemas.Sandwich, tags=["sandwiches"])
+def delete_sandwich(sandwich_id: int, db: Session = Depends(get_db)):
+    db_sandwich = sandwiches_controller.read_one_sandwich(db=db, sandwich_id=sandwich_id)
+    if db_sandwich is None:
+        raise HTTPException(status_code=404, detail="Sandwich not found")
+    return sandwiches_controller.delete_sandwich(db=db, sandwich_id=sandwich_id)
